63c388622723f501e40ce0675e761e868c303a72
jgfurlan <joaogfurlan@hotmail.co.uk>
2023-11-10 04:33:35 +0000

    implementing EnvConfig Interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EnvConfigModule, ConfigModule],
+  imports: [EnvConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [EnvConfigService]
+  providers: [ConfigService]
 })
 export class EnvConfigModule {}
